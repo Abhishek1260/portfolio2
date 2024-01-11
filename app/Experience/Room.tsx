098edc41393ead280d3837f38d733aa0852c8bc7
Abhishek Bansal <abhishekbansal938@gmail.com>
2024-01-11 17:40:19 +0530

    final commit

--- app/Experience/Room.tsx
+++ app/Experience/Room.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { OrbitControls, useGLTF } from "@react-three/drei"
-import { useLoader } from "@react-three/fiber"
+import { useLoader, useThree } from "@react-three/fiber"
 import { TextureLoader } from 'three'
 import * as THREE from 'three'
 import { useEffect } from "react"
@@ -11,6 +11,7 @@ export const Room = () => {
 
     const { scene } = useGLTF("/portfolio.glb")
     const colorMap = useLoader(TextureLoader, "/BakedTexture20.png")
+    colorMap.encoding = THREE.sRGBEncoding
     colorMap.flipY = false
 
     scene.traverse((e) => {
