0f6f5a0e74da2285da6ff11a038aae1cae4702fa
Abhishek Bansal <abhishekbansal938@gmail.com>
2024-01-11 15:26:08 +0530

    final commit

--- app/Experience/Experience.tsx
+++ app/Experience/Experience.tsx
@@ -2,13 +2,16 @@
 
 import { Canvas } from "@react-three/fiber"
 import { Room } from "./Room"
+import { Suspense } from "react"
 
 export const Experience = () => {
     return <>
 
-        <Canvas>
+        <Canvas >
 
-            <Room />
+            <Suspense>
+                <Room />
+            </Suspense>
 
         </Canvas>
 
--- app/Experience/Room.tsx
+++ app/Experience/Room.tsx
@@ -1,11 +1,29 @@
 "use client"
 
 import { OrbitControls, useGLTF } from "@react-three/drei"
+import { useLoader } from "@react-three/fiber"
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import * as THREE from 'three'
+import { useEffect } from "react"
 
 
 export const Room = () => {
 
     const { scene } = useGLTF("/portfolio.glb")
+    const colorMap = useLoader(TextureLoader, "/BakedTexture6.png")
+    colorMap.flipY = false
+
+    scene.traverse((e) => {
+        if (e instanceof THREE.Mesh) {
+            e.material = new THREE.MeshBasicMaterial({ map: colorMap })
+        }
+    })
+
+    useEffect(() => {
+
+    }, [])
+
+
 
     return <>
         <pointLight position={[-5, 5, 0]} intensity={100} color={"#ffffff"} />
