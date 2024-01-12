c23ce2409e0e9bf0c3beadc9a09fb4276c7b8c6c
Abhishek Bansal <abhishekbansal938@gmail.com>
2024-01-12 21:15:01 +0530

    final commit

--- app/Experience/Room.tsx
+++ app/Experience/Room.tsx
@@ -4,21 +4,34 @@ import { OrbitControls } from "@react-three/drei"
 import { Suspense } from "react"
 import { SceneOne } from "./SceneOne"
 import { SceneFour } from "./SceneFour"
+import { SceneTwo } from "./SceneTwo"
+import { SceneThree } from "./SceneThree"
+import { useFrame, useThree } from "@react-three/fiber"
 
 
 export const Room = () => {
 
+    useFrame(({ camera }) => {
 
+        camera.position.set(-15, 13, 15)
+
+    })
 
 
     return <>
         <ambientLight intensity={100} color={"#ffffff"} />
-        <OrbitControls />
         <Suspense>
             <SceneOne />
         </Suspense>
         <Suspense>
             <SceneFour />
+        </Suspense>
+        <Suspense>
+            <SceneTwo />
+        </Suspense>
+
+        <Suspense>
+            <SceneThree />
         </Suspense>
 
     </>
---
+++ app/Experience/SceneThree.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useGLTF } from "@react-three/drei"
+import { useLoader } from "@react-three/fiber"
+import { TextureLoader } from 'three'
+import * as THREE from 'three'
+
+export const SceneThree = () => {
+
+    const { scene } = useGLTF("/texture3.glb")
+    const colorMap = useLoader(TextureLoader, "/Texture3.jpg")
+    colorMap.encoding = THREE.sRGBEncoding
+    colorMap.flipY = false
+
+    scene.traverse((e) => {
+        if (e instanceof THREE.Mesh) {
+            e.material = new THREE.MeshBasicMaterial({ map: colorMap })
+        }
+    })
+
+
+
+    return <>
+
+        <primitive object={scene} />
+
+    </>
+
+}
